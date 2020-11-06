1ac74565c4fbf22a9f279941035cbf4631244b08
Ryan Ermita <ryan.ermita@uploan.ph>
2020-11-06 15:05:10 +0800

    this should work, please.

--- app.py
+++ app.py
@@ -2,6 +2,4 @@ def is_odd(number):
     print(
         f"the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog the quick brown fox jumps over the lazy dog"
     )
-
-
-return number % 2 == 0
+    return number % 2 == 0
